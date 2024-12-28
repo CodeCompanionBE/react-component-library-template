a264eab598ad7942e2ead0d7641c4f73ee64c33e
niels-van-den-broeck <vandenbroeckniels@gmail.com>
2024-12-28 11:03:08 +0100

    add storybook

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import tsc from '@rollup/plugin-typescript';
+import postcss from 'rollup-plugin-postcss';
+import autoprefixer from 'autoprefixer';
 import * as glob from 'glob';
 import pkg from './package.json' with { type: 'json' };
 
@@ -18,12 +20,34 @@ const generateRegexFromPeerDependencies = () => {
 const config = {
 	input: globImportFiles({ extensions: ['ts', 'tsx'] }),
 	external: generateRegexFromPeerDependencies(),
-	plugins: [tsc()],
+	plugins: [
+		tsc({
+			tsconfig: './tsconfig.build.json',
+			sourceMap: true,
+			// inlineSources: true,
+			declaration: true,
+			declarationDir: 'build',
+			rootDir: 'src',
+			exclude: [
+				'src/**/*.test.ts',
+				'src/**/*.test.tsx',
+				'src/**/*.stories.tsx',
+			],
+			// include: ['src/**/*(!.test).ts', 'src/**/*(!.test).tsx'],
+		}),
+		postcss({
+			plugins: [autoprefixer()],
+			sourceMap: true,
+			extract: true,
+			minimize: true,
+		}),
+	],
 	output: {
 		dir: 'build',
 		format: 'esm',
 		preserveModules: true,
 		preserveModulesRoot: 'src',
+		sourcemap: true,
 	},
 };
 
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,11 +1,32 @@
 import type { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
+import styles from './button.module.css';
+import clsx from 'clsx';
+
+export type ButtonProps = DetailedHTMLProps<
+	ButtonHTMLAttributes<HTMLButtonElement>,
+	HTMLButtonElement
+> & {
+	variant?: 'primary' | 'secondary';
+};
 
 export default function Button({
 	children,
+	className,
+	type = 'button',
+	variant = 'primary',
 	...props
-}: DetailedHTMLProps<
-	ButtonHTMLAttributes<HTMLButtonElement>,
-	HTMLButtonElement
->) {
-	return <button {...props}>{children}</button>;
+}: ButtonProps) {
+	return (
+		<button
+			className={clsx(className, {
+				[styles.button]: true,
+				[styles.primary]: variant === 'primary',
+				[styles.secondary]: variant === 'secondary',
+			})}
+			type={type}
+			{...props}
+		>
+			{children}
+		</button>
+	);
 }
